554838dfaf47e9461a391587e4e9303fb901b4a1
garv-iitr <garv@cs.iitr.ac.in>
2025-12-03 13:22:55 +0530

    ETE

--- ete.c
+++ ete.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main() {
-static char* a,b,c,d ;
-printf("a =%d %d\n",sizeof(a));
-printf("b =%d %d\n",sizeof(b));
-printf("c =%d %d\n",sizeof(c));
-printf("d =%d %d\n",sizeof(d));
-return 0;
+
+    printf("Hello World\n");
+
+
+    return 0;
 }
